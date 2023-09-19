8128f3598b2f59d3eb18698d179808e08484212d
Clyde95 <clydekambamura@icloud.com>
2023-09-19 12:06:06 +0200

    The components have been set up

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import CustomerReviews from "./CustomerReviews";
+import SuperQuality from "./SuperQuality";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+import Footer from "./Footer";
+import Subscribe from "./Subscribe";
+
+export {
+    Hero,
+    PopularProducts,
+    Services,
+    SpecialOffer,
+    Subscribe,
+    SuperQuality,
+    Footer,
+    CustomerReviews
+}
