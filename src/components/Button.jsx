bcc5d31697bb6d0258fd0af1ad68edef5c01c626
Clyde95 <clydekambamura@icloud.com>
2023-10-27 13:16:29 +0200

    Done with Superquality

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -8,11 +8,12 @@ const Button = ({ label, iconURL}) => {
     rounded-full text-white
     border-coral-red">
       {label}
-      <img src={iconURL} 
+
+      {iconURL && <img src={iconURL} 
       alt="arrown right icon"
       className="ml-2 rounded-full
       w-5 h-5" 
-      />
+      />}
 
     </button>
   )
